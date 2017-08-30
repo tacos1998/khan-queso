00c354164417d9d9d0b1e24a5dcf2cbf15347f0e
Jeremie <amanojeremie@gmail.com>
2017-08-30 00:52:35 -0500

    Changed nunjucks to pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,52 @@
-const gulp = require("gulp");
-const del = require("del");
-const sass = require("gulp-sass");
-const nunjucks = require("gulp-nunjucks");
-const browserSync = require("browser-sync");
+const gulp = require('gulp');
+const del = require('del');
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync');
 
-gulp.task("clean", function() {
-	return del(["build/"]);
+gulp.task('clean', function() {
+	return del(['build/']);
 });
 
-gulp.task("includes", function() {		
-	gulp.src("includes/**/*.*")
-		.pipe(gulp.dest("build/"))
+gulp.task('includes', function() {		
+	gulp.src(['includes/**/*'])
+		.pipe(gulp.dest('build/'))
 		.pipe(browserSync.reload({
 			stream: true
 		}));
 });
 
-gulp.task("sass", function() {
-	gulp.src("src/sass/**/*.scss")
-		.pipe(sass().on("error", sass.logError))
-		.pipe(gulp.dest("build/css/"))
+gulp.task('sass', function() {
+	gulp.src('src/sass/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest('build/css/'))
 		.pipe(browserSync.reload({
 			stream: true
 		}));
 });
 
-gulp.task("nunjucks", function() {
-	gulp.src("src/njk/**/*.html")
-		.pipe(nunjucks.compile())
-		.pipe(gulp.dest("build/"))
+gulp.task('pug', function() {
+	gulp.src(['src/pug/**/*.pug', '!src/pug/**/_*.pug'])
+		.pipe(pug())
+		.pipe(gulp.dest('build/'))
 		.pipe(browserSync.reload({
 			stream: true
 		}));
 });
 
-gulp.task("browserSync", function() {
+gulp.task('browserSync', function() {
 	browserSync({
 		server: {
-			baseDir: "build"
+			baseDir: 'build'
 		}
 	});
 });
 
-gulp.task("default", ["includes", "sass", "nunjucks"], function() {
+gulp.task('default', ['includes', 'sass', 'pug'], function() {
 });
 
-gulp.task("watch", ["browserSync", "includes", "sass", "nunjucks"], function() {
-	gulp.watch("src/sass/**/*.scss", ["sass"]);
-	gulp.watch("src/njk/**/*.*", ["nunjucks"]);
-	gulp.watch("includes/**/*.*", ["includes"]);
+gulp.task('watch', ['browserSync', 'includes', 'sass', 'pug'], function() {
+	gulp.watch('src/sass/**/*.scss', ['sass']);
+	gulp.watch('src/pug/**/*.*', ['pug']);
+	gulp.watch('includes/**/*.*', ['includes']);
 });
